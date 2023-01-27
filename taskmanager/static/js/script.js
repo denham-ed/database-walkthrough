7c6131aa7060156dbb1e66c396e9aafc57e4f617
Ed Denham <ed.denham@gmail.com>
2023-01-27 03:35:18 +0000

    Add New Task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
     //Nav Side Bar
     $('.sidenav').sidenav();
   });
-      
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let datepicker = document.querySelectorAll('.datepicker');
+     M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+  });
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
